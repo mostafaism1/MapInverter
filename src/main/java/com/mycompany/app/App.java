08f13891e04903ea41307f3cac0df39cfaacba8c
mostafa <me.cssign@gmail.com>
2021-10-17 19:55:15 +0200

    Write a main function that handles reading a JSON file, converting it into a Map object, inverting the Map, then finally saving the inverted map to a file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,39 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws JsonParseException, JsonMappingException, IOException
     {
-        System.out.println( "Hello World!" );
+        String sourcePath = "map.json";
+        String destinationPath = "inverterMap.json";
+        Map<String, List<String>> map = readJsonFromFile(sourcePath);
+        MapInverter<String, String> mapInverter = new MapInverter<String, String>(map);
+        Map<String, List<String>> invertedMap = mapInverter.invert();
+        writeJsonToFile(invertedMap, destinationPath);
+    }
+
+    private static Map<String, List<String>> readJsonFromFile(String sourcePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, List<String>> map =  objectMapper.readValue(new File(sourcePath), new TypeReference<Map<String,List<String>>>(){});
+        return map;
+    }
+
+    private static void writeJsonToFile(Map<String, List<String>> invertedMap, String destinationPath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(destinationPath), invertedMap);
     }
 }
