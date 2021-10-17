570de89e644f5e16a0d40fc6d97066953c940f14
mostafa <me.cssign@gmail.com>
2021-10-17 20:38:52 +0200

    Remove fine-grained exception handling

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,18 +5,12 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args ) throws JsonParseException, JsonMappingException, IOException
+    public static void main( String[] args ) throws IOException
     {
         String sourcePath = "map.json";
         String destinationPath = "inverterMap.json";
